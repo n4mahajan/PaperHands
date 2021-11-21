c31bbae446e2aabfe084466da22d921a26902b1a
nmahajan <n4mahajan@gmail.com>
2021-11-21 12:44:46 -0500

    Add funds functionality

--- PaperHands/Profile/Profile.js
+++ PaperHands/Profile/Profile.js
@@ -7,7 +7,7 @@
 //
 
 import React,{useContext,useEffect,useState} from "react"
-import { Image, StyleSheet, Text, View ,Button,TextInput,FlatList} from "react-native"
+import { Image, StyleSheet, Text, View ,Button,TextInput, TouchableOpacity, Alert} from "react-native"
 import { AuthContext } from "../../context/AuthProvider"
 import firebase from "firebase"
 
@@ -16,11 +16,46 @@ import SearchBar from "../../components/SearchBar"
 export default function Profile () {
 	const {user,balance,stocks}= useContext(AuthContext)
 
+	const [amount, setAmount] = useState()
+
+	async function addFunds() {
+		if (isNaN(amount)) {
+			Alert.alert("Please enter a valid amount of funds to add", "", [
+				{text: "Okay", onPress: () => console.log("Transaction Voided")},
+			])
+			setAmount();
+			return;
+		}
+		
+		const funds = parseFloat(amount)
+
+		await firebase.firestore().collection("Users").doc(user.uid).update({
+			balance: balance + funds
+		})
+
+		setAmount()
+	}
 
 	return (
-		<View>
-			<Text>Hello user {user.name}</Text>
-			<Text>Your balance is {balance.toFixed(2)}</Text>
+		<View style={{width: "95%", alignSelf: "center", flex: 1}}>
+			<Text style={{alignSelf: "center", fontWeight: "bold", fontSize: 22, marginBottom: "5%"}}>Hello, {user.name}</Text>
+			<View style={styles.header}>
+				<Text>Finances</Text>
+			</View>
+			<Text>Your balance is: ${balance}</Text>
+			<View style={{flexDirection: "row", marginTop: "3%", marginBottom: "5%"}}>
+				<Text style={{marginRight: "3%", marginTop: "2%"}}>Add funds:</Text>
+				<TextInput placeholder="Amount" value={amount} onChangeText={setAmount} style={styles.textInput} keyboardType="numeric"/>
+			</View>
+			<TouchableOpacity style={styles.button} onPress={() => 
+				Alert.alert("Are you sure you want to add funds?", "", [
+					{text: "Confirm", onPress: () => {
+						addFunds()
+					}},
+					{text: "Deny", onPress: () => console.log("Transaction Voided")},
+				])}   >
+				<Text style={styles.buttonText}>Add</Text>
+			</TouchableOpacity>
 			<Button title="sign out" onPress={()=>{
 				firebase.auth().signOut()
 			}}/>
@@ -28,3 +63,39 @@ export default function Profile () {
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	header: {
+		borderRadius: 10,
+		borderWidth: 0.2,
+		width: "35%",
+		alignItems: "center",
+		marginBottom: "5%",
+		backgroundColor: "#00ffa9",
+		paddingTop: "1%",
+		paddingBottom: "1%"
+	},
+	textInput: {
+		borderColor: "#000000",
+		borderWidth: 1,
+		width: "75%",
+		height: "140%",
+		paddingLeft: "3%"
+	},
+	button: {
+		borderRadius: 10,
+		borderWidth: 0.2,
+		width: "35%",
+		alignItems: "center",
+		backgroundColor: "black",
+		marginLeft: "40%",
+		height: "4%",
+		justifyContent: "center"
+	},
+	buttonText: {
+		color: 'white',
+		fontWeight: 'bold',
+		fontSize: 16,
+		textAlign: 'center',
+	}
+})
